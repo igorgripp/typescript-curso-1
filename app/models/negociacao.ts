cfa531df8f55e16190122c1593f71b5fe3c4c2ac
unknown <igorgripp@gmail.com>
2021-11-23 17:23:24 -0300

    Finalização da primeira parte do curso de TypeScript

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,28 +1,18 @@
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
-
-  constructor(data, quantidade, valor){
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
-  }
-
-  get data(){
-    return this.#data;
-  }
-
-  get quantidade(){
-    return this.#quantidade;
-  }
-
-  get valor(){
-    return this.#valor;
-  }
+  
+  constructor(
+    private _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number
+  ){}
 
   get volume(){
-    return this.#quantidade * this.#valor;
+    return this.quantidade * this.valor;
+  }
+
+  get data(): Date{
+    const data = new Date(this._data.getTime());
+    return data;
   }
 
 
